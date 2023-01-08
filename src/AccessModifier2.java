3dce369d78c4b426a40621f6208b4346c06a60e7
Your Name <you@example.com>
2023-01-08 09:39:11 +0700

    Access Modifier 2

---
+++ src/AccessModifier2.java
@@ -0,0 +1,21 @@
+public class AccessModifier2 {
+    private String name = "John";
+    private String cLass= "CO2";
+    public AccessModifier2(){};
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void setcLass(String cLass)
+    {
+        this.cLass=cLass;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getcLass() {
+        return this.cLass;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        AccessModifier2 am = new AccessModifier2();
+        am.setName("Nai");
+        am.setcLass("10");
+        System.out.println(am.getName());
+        System.out.println(am.getcLass());
+    }
+}
